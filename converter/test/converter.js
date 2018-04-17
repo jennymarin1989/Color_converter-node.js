f8a764f9db1de76081781ea5c76a52ce1c5984ca
jose <jennymarin1989@hotmail.com>
2018-04-17 23:23:57 +0100

    Add first test for color code converter

---
+++ converter/test/converter.js
@@ -0,0 +1,12 @@
+const expect = require("chai").expect;
+const converter= require("../app/converter");
+
+
+describe("Color Code Converter", function(){
+  describe("RGB to Hex conversion", function(){
+    it("converts the basic colors", function(){
+			var regHex = converter.rgbToHex(255,0,0);
+			expect(regHex).to.equal("ff0000")
+    });
+  });
+});
