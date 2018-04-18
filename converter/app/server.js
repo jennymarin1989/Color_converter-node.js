6d0a4759dc54a66478a61e284a4a000aacc668af
jose <jennymarin1989@hotmail.com>
2018-04-18 20:30:10 +0100

    Pass test for hex color conversion to RGB

--- converter/app/server.js
+++ converter/app/server.js
@@ -13,6 +13,13 @@ app.get("/rgbToHex", function(req, res){
 	
 	var hex = converter.rgbToHex(red, green, blue);
 	res.send(hex);
+});
+
+app.get("/hexToRgb", function(req, res){
+	var hex = req.query.hex
+	var rgb = converter.hexToRgb(hex);
+
+	res.send(JSON.stringify(rgb)); // Returns a Json string representing the given value.
 })
 
 app.listen(3000);
