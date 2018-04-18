490ab8625a9b91102b1d27422d62f3f200ed30f2
jose <jennymarin1989@hotmail.com>
2018-04-18 12:38:18 +0100

    Add server.js file in /test - Add test to connect to server

---
+++ converter/test/server.js
@@ -0,0 +1,23 @@
+const expect = require("chai").expect;
+const request = require("request");
+
+describe("Color code converter API", function(){
+   
+  var url = "http://localhost:3000/rgbToHex?red=255&green=255&blue=255";
+   
+  describe("RGB to Hex conversion", function(){
+    it("returns status 200", function(){
+      request(url, function(error, response, body){
+				expect(response.statusCode).to.equal(200);
+				done();
+      });
+    });
+    it("returns the color in hex", function(){
+			request(url, function(error, reponse, body){
+				expect(body).to.equal("ffffff");
+				done();
+			});
+		});
+	});
+	
+})
